6151b347cafc00faa314a219d03ba13f3d417850
Anmolsharma786 <52050021+Anmolsharma786@users.noreply.github.com>
2020-05-28 15:46:32 -0400

    Third Commit

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('Hello world');
+
+console.log("***");
+console.log("Computer Chooses" + computerResult); 
+for (var index = 0; index <= 100; index++) {
+    var randomPick = getRandomInt(0, 3);
+    var computerResult = possibleAnswer[randomPick];
+    compare(ourAnswer, computerResult);    
+}
